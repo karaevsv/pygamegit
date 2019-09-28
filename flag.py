1f5e9447318488a34f61defc1ab2a6bd1520217d
karaevsv <karaevsv@gmail.com>
2019-09-28 21:48:38 +0300

    Нарисоан белый прямоугольник.

--- flag.py
+++ flag.py
@@ -8,6 +8,7 @@ screen = pygame.display.set_mode(size)
 
 def draw():
     pygame.draw.rect(screen, pygame.Color('brown'), [(0, 0), (20, 600)], 0)
+    pygame.draw.rect(screen, pygame.Color('white'), [(20, 0), (380, 150)], 0)
 
 
 draw()
