43995f275eb7565731e95de4d80fa503d5d56b3a
karaevsv <karaevsv@gmail.com>
2019-09-28 21:49:59 +0300

    Нарисоан синий прямоугольник.

--- flag.py
+++ flag.py
@@ -8,6 +8,7 @@ screen = pygame.display.set_mode(size)
 
 def draw():
     pygame.draw.rect(screen, pygame.Color('brown'), [(0, 0), (20, 600)], 0)
+    pygame.draw.rect(screen, pygame.Color('blue'), [(20, 150), (380, 150)], 0)
 
 
 draw()
